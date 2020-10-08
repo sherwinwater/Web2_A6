d257b19efb77ae3b32c141354a881702d2c3e5a1
sherwin <sherwin.water@gmail.com>
2020-10-08 12:43:44 -0400

    new

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php include_once("index.html") ;?>
+<?php
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <div>
+            <a href="productApple.php" style="font-size: 2em;">Order Your Fruit</a>
+            <img src="./img/fruit.jpeg" alt="fruit" id="fruit" style="width:800px; height:500px;" >
+        </div>
+    </body>
+</html>
